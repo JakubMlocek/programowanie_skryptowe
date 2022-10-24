4a183986273865c00b8c759e052c1ef9a18ea2fd
JakubMlocek <jakub.mlocek.informatyka@gmail.com>
2022-10-24 10:38:16 +0200

    update in lab3

---
+++ lab3/dom/zad2/DeanerySystem/day.py
@@ -0,0 +1,32 @@
+import enum
+
+class Day(enum.Enum):
+    MON = 0
+    TUE = 1
+    WED = 2
+    THU = 3
+    FRI = 4
+    SAT = 5
+    SUN = 6
+
+    def readable(self):
+        return {
+        Day.MON : "Poniedziałek",
+        Day.TUE : "Wtorek",
+        Day.WED : "Środa",
+        Day.THU : "Czwartek",
+        Day.FRI : "Piątek",
+        Day.SAT : "Sobota",
+        Day.SUN : "Niedziela"
+        }[self]
+    
+    def difference(self, day):
+        diff = day.value - self.value
+        if(diff > 4):
+            diff -= 7
+        elif(diff < -4):
+            diff += 7
+        return diff
+
+def nthDayFrom(n, day):
+    return Day((day.value + n)%7)
---
+++ lab3/dom/zad2/DeanerySystem/term.py
@@ -0,0 +1,32 @@
+from DeanerySystem.day import Day
+
+class Term():
+    def __init__(self,day :Day, hour :int, minute :int) -> None:    
+        self.hour = hour
+        self.minute = minute
+        self.duration = 90
+        self.__day = day
+
+    def __str__(self) -> str:
+        return f"{self.__day.readable()} {self.hour}:{self.minute} [{self.duration}]" 
+
+    def __int__(self):
+        return (self.__day.value*1440+self.hour*60+self.minute)
+
+    def earlierThan(self, termin):
+        if int(self) < int(termin):
+            return True
+        else:
+            return False
+
+    def laterThan(self, termin):
+        if int(self) > int(termin):
+            return True
+        else:
+            return False
+        
+    def equals(self, termin):
+        if int(self) == int(termin):
+            if self.duration == termin.duration:
+                return True
+        return False
